2126bf16e2bd8bd7c6adc0daa80aa6b75c86fdc4
samantha <samanthapelkova1@gmail.com>
2023-12-12 10:20:01 +0100

    adding router link and new file

---
+++ src/views/PostDetail.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+    <p>Display the content of post with ID of {{ $route.params.id }} here!</p>
+    <p><router-link to="/posts">&lt; Back</router-link></p>
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+
+    <ul>
+      <li>
+        <router-link to="/postDetail/id1">Post 1</router-link>
+      </li>
+    </ul>
+
+    <ul>
+      <li>
+        <router-link to="/postDetail/id2">Post 2</router-link>
+      </li>
+    </ul>
+
+    <ul>
+      <li>
+        <router-link to="/postDetail/id3">Post 3</router-link>
+      </li>
+    </ul>
+
+    <ul>
+      <li>
+        <router-link to="/postDetail/id4">Post 4</router-link>
+      </li>
+    </ul>
+    <textarea v-autofocus/>
+  </div>
+</template>
+
+<script setup>
+import { vAutofocus } from "@/Directives/vAutofocus.js"
+</script>
+
+<style scoped>
+ul{
+  margin-bottom: 30px;
+}
+</style>
